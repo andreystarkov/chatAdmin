abacf5f2e63650a3399882c6f4ca0ca69e8004f8
Andrey Starkov <admstarkov@sberbank.ru>
2020-04-14 15:26:55 +0300

    clean

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  token: '1075517277:AAEpUZp2m72f6KvdekwxOQ-MwHNmqjUugdY',
+  token: '1189327662:AAGYC2cycDx8AFpFIDFpIZk9491KabM_cxQ',
   mongoLink: 'mongodb+srv://chat:admin@cluster0-g4hlr.mongodb.net/iktzi?retryWrites=true&w=majority',
   chats: [-1001381307542, -1001166073886, -1001410544997], // your chats
   botId: 1075517277, // bot id
@@ -12,7 +12,7 @@ module.exports = {
     d: 86400,
     w: 604800
   },
-  
+
   timeNames: { // you can change lang.
     m: 'мин.',
     h: 'час.',
